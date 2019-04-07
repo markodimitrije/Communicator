252077d027efcb2b4188aae92c8b6a0f2570ec2b
markodimitrije <markodimitrije@gmail.com>
2019-04-07 10:43:52 +0200

    sound assets for drinks

--- MyEnvironment/Model/Data.swift
+++ MyEnvironment/Model/Data.swift
@@ -14,37 +14,48 @@ struct RawDataModel {
                       "Animals", "Transport", "Activities", "Rewards"]
     
     var categories: [String: [String]] = [
-        "Food": ["apple", "banana", "spagetthi", "cake",
-                 "cookies", "chocolate", "popcorn", "smoki",
+        "Food": ["apple", "banana", "cake", "cookies",
+                 "chocolate", "popcorn", "smoki", "spagetthi",
                  "soup", "meat", "frenchfries", "sandwich"],
+        
         "Drinks": ["beer", "cedevita", "cocacola", "coffie",
                    "lemonade", "milk", "nestea", "orangejeuce",
                    "soda", "tea", "water", "yogurt"],
+        
         "Clothes": ["cap", "shal", "gloves", "jacket",
                     "pijamas", "pulover", "shirt", "shorts",
                     "socks", "tshirt", "underpants", "pants"],
+        
         "Shoes": ["boots", "sandals", "shoes", "sleepers", "sneakers"],
+        
         "Objects": ["backpack", "blanket", "chair", "computer",
                     "desk", "hanger", "mirror", "mobile",
                     "pillow", "clipper", "shelf", "tv"],
+        
         "SchoolEquipment": ["acrylicpaint", "backpack", "books", "flomaster",
                             "glue", "notes", "pencil", "pencilbox",
                             "racunaljka", "rubber", "scissors", "sharpener"],
+        
         "Bathroom": ["bathtub", "hairbrush", "hairdryer", "shampoo",
                      "showercabin", "soap", "toaletpaper", "toilet",
                      "toothbrush", "toothpaste", "towel", "washbasin"],
+        
         "Kitchen": ["bowl", "cooker", "cup", "fork",
                     "fridge", "glass", "kitchensink", "knife",
                     "plate", "pot", "showcase", "spoon"],
+        
         "Transport": ["bike", "boat", "bus", "car",
                       "motorbike", "plane", "rollers", "ship",
                       "train", "tram", "trotinet", "truck"],
+        
         "Activities": ["blowingbubbles", "jumpingball", "jumpingontrampoline", "musiclistening",
                        "painting", "playingbasketball", "readingbook", "ridingbike",
                        "ridingrollers", "swimming", "toyplaying", "watchingtv"],
+        
         "Rewards": ["car", "cartoons", "grisine", "handcream",
                     "juice", "jumpingball", "mobilegames", "music",
                     "plastelin", "popcorn", "scotchtape", "smoki"],
+        
         "Animals": ["dog", "cat", "horse", "rabbit",
                     "rooster", "chicken", "cow", "goat",
                     "sheep", "bear", "monkey", "mouse", "bird"]
